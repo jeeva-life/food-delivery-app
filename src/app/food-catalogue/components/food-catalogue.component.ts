3f386f21a30b853cb9cbf076e3390006c91b8b02
jeeva-life <kodalijeevan756@gmail.com>
2024-09-30 17:00:33 -0400

    Normalize line endings

---
+++ src/app/food-catalogue/components/food-catalogue.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-food-catalogue',
+  templateUrl: './food-catalogue.component.html',
+  styleUrl: './food-catalogue.component.css'
+})
+export class FoodCatalogueComponent {
+
+}
